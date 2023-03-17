7a5e1f0cd63154e54e16967065681e1234bf2619
Lastiada <lastiada1@gmail.com>
2023-03-17 13:03:43 +1000

    add pop up button

--- src/pages/spot_list/index.jsx
+++ src/pages/spot_list/index.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import { Box, useTheme } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -31,6 +37,16 @@ const SpotList = () => {
     { field: "city", headerName: "City", width: 100 },
     { field: "zipCode", headerName: "Zip Code", width: 100 },
   ];
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
@@ -75,6 +91,62 @@ const SpotList = () => {
           components={{ Toolbar: GridToolbar }}
         />
       </Box>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <div>
+          <Button variant="contained" onClick={handleClickOpen}>
+            Open alert dialog
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Use Google's location service?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Let Google help apps determine location. This means sending anonymous
+                location data to Google, even when no apps are running.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Disagree</Button>
+              <Button onClick={handleClose} >
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </Box>
+      {/* <div>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Open alert dialog
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Use Google's location service?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Let Google help apps determine location. This means sending anonymous
+                location data to Google, even when no apps are running.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Disagree</Button>
+              <Button onClick={handleClose} autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
+      </div> */}
     </Box>
   );
 };
