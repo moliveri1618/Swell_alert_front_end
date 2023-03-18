f8af952cde65c1b6bd668d48ddf9a67121d3195b
Mauro <lastiada1@gmail.com>
2023-03-18 22:59:08 +1000

    calendar finish

--- src/pages/fly_tickets/index.jsx
+++ src/pages/fly_tickets/index.jsx
@@ -15,76 +15,106 @@ import {
 } from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
+import Chip from "@material-ui/core/Chip";
 
-const AllEvents = [
-    {
-        id: "12315",
-        title: "All-day event",
-        date: "2023-03-14",
-    },
-    {
-        id: "5123",
-        title: "Timed event",
-        date: "2022-09-28",
-    },
-    {
-        id: "111",
-        title: "Ciaso",
-        date: "2022-09-11",
-    },
-    {
-        id: "1231",
-        title: "Ciaso",
-        date: "2022-09-11",
-    }
-];
+const LocationTags = ({ tags, handleTagClick }) => (
+    <Box display="flex" flexWrap="wrap" alignItems="center" mb="20px">
+        {tags.map((tag) => (
+            <Chip
+                key={tag}
+                label={tag}
+                mr="10px"
+                mb="10px"
+                mx="10px"
+                my="10px"
+                onClick={() => handleTagClick(tag)}
+            />
+        ))}
+    </Box>
+);
+
+
 
 const FlyTickets = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const [currentEvents, setCurrentEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState(null);
+
+    const allEvents = [
+        {
+            id: "12315",
+            title: "All-day event",
+            date: "2023-03-14",
+            location: "Bali"
+        },
+        {
+            id: "5123",
+            title: "Timed event",
+            date: "2023-03-15",
+            location: "Bali"
+        },
+        {
+            id: "111",
+            title: "Ciaso",
+            date: "2023-03-16",
+            location: "Spain"
+        }
+    ];
 
     //calculate height of div based on n events
     const [listHeight, setListHeight] = useState(0);
     const listRef = useRef(null);
-    const maxHeight = 800;
+    const maxHeight = 950;
 
-    const handleDateClick = (selected) => {
-        const title = prompt("Please enter a new title for your event");
-        const calendarApi = selected.view.calendar;
-        calendarApi.unselect();
+    //create an array of unique locations
+    const locations = Array.from(new Set(allEvents.map(event => event.location)));
 
-        if (title) {
-            calendarApi.addEvent({
-                id: `${selected.dateStr}-${title}`,
-                title,
-                start: selected.startStr,
-                end: selected.endStr,
-                allDay: selected.allDay,
-            });
-        }
+    // Define state variables to keep track of the selected tag and filtered notes
+    const [events, setEvents] = useState(allEvents);
+
+    // // Define function to handle tag clicks
+    const handleTagClick = (location) => {
+        setEvents(allEvents.filter((event) => event.location.includes(location)));
+        setFilteredEvents(allEvents.filter((event) => event.location === location));
     };
 
-    const handleEventClick = (selected) => {
-        if (
-            window.confirm(
-                `Are you sure you want to delete the event '${selected.event.title}'`
-            )
-        ) {
-            selected.event.remove();
-        }
-    };
+
+    // const handleDateClick = (selected) => {
+    //     const title = prompt("Please enter a new title for your event");
+    //     const calendarApi = selected.view.calendar;
+    //     calendarApi.unselect();
+
+    //     if (title) {
+    //         calendarApi.addEvent({
+    //             id: `${selected.dateStr}-${title}`,
+    //             title,
+    //             start: selected.startStr,
+    //             end: selected.endStr,
+    //             allDay: selected.allDay,
+    //         });
+    //     }
+    // };
+
+    // const handleEventClick = (selected) => {
+    //     if (
+    //         window.confirm(
+    //             `Are you sure you want to delete the event '${selected.event.title}'`
+    //         )
+    //     ) {
+    //         selected.event.remove();
+    //     }
+    // };
 
     useEffect(() => {
         const listHeight = listRef.current.getBoundingClientRect().height;
         setListHeight(listHeight);
-        console.log(listHeight)
-    }, [currentEvents]);
+    }, [events]);
 
 
     return (
         <Box m="20px">
             <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+            <LocationTags tags={locations} handleTagClick={handleTagClick} />
             <Grid container spacing={2}>
                 <Grid xs={12} md={4}>
                     <Box
@@ -95,7 +125,7 @@ const FlyTickets = () => {
                         <Typography variant="h5">Events</Typography>
                         <div style={{ height: `min(${listHeight}px, ${maxHeight}px)`, overflow: 'auto' }}>
                             <List ref={listRef}>
-                                {currentEvents.map((event) => (
+                                {events.map((event) => (
                                     <ListItem
                                         key={event.id}
                                         sx={{
@@ -138,16 +168,16 @@ const FlyTickets = () => {
                                 right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
                             }}
                             initialView="dayGridMonth"
-                            editable={true}
-                            selectable={true}
+                            // editable={true}
+                            // selectable={true}
                             selectMirror={true}
                             dayMaxEvents={true}
                             // select={handleDateClick}
                             // eventClick={handleEventClick}
-                            eventsSet={(events) => setCurrentEvents(events)}
-                            initialEvents={AllEvents}
+                            //eventsSet={(events) => { setEvents(events); }}
+                            initialEvents={allEvents}
                             eventContent={(eventInfo) => {
-                                console.log(eventInfo)
+                                //console.log(eventInfo.event)
                                 return (
                                     <div
                                         style={{
@@ -158,7 +188,8 @@ const FlyTickets = () => {
                                             fontWeight: 'bold', // add bold font weight to the event tag
                                         }}
                                     >
-                                        {eventInfo.timeText} {eventInfo.event.title}
+                                        {eventInfo.event.title} <br />
+                                        {eventInfo.event.extendedProps.location}
                                     </div>
                                 );
                             }}
